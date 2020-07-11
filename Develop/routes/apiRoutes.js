01162ae6f9a85625464d1b5c78ee35afb0cbaf34
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-11 16:11:21 -0500

    added in the id Counter variable and the full api get request

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -8,15 +8,21 @@ var path = require("path");
 const database = path.resolve(__dirname, "../db");
 
 // Will I need to start the counter with a variable?? 
-// let idCounter or let idNumber =1;  ???
+let idCounter = 1;
 
 //API routes here
 module.exports = function (app) {
-    //API GET
+    //API GETrequest for the notes in database file
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(path.resolve(database, "db.json"), "utf8", function (err, data) {
+            res.json(JSON.parse(data));
+        });
+    });
 
+    //API POST for notes in the database file
+    app.post("/api/notes", function (req, res) {
 
-
-    //API POST
+    })
 
 
 
