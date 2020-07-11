29a93dfd3ea30b9c0a8aadebc81b175bf16b9ee4
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-11 15:49:59 -0500

    started the large module export that will have the GET POSt and DELETE api requests

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,1 +1,29 @@
-//Need fs and path require here
+//Need fs require here
+var fs = require("fs");
+
+// Need path require here  for directory 
+var path = require("path");
+
+//Need a db directory and pathway?
+
+
+// Will I need to start the counter with a variable?? 
+// let idCounter or let idNumber =1;  ???
+
+//API routes here
+module.exports = function (app) {
+    //API GET
+
+
+
+    //API POST
+
+
+
+    //API DELETE
+
+
+
+}
+
+
